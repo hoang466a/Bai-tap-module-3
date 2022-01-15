60bc7952ffca8d46c4530ace70c5dc2f71b42adc
hoang466a <hoangtn96@gmail.com>
2022-01-15 21:58:45 +0700

    day bai tap ngay 15/01/2022

--- s3/baitap/b1.sql
+++ s3/baitap/b1.sql
@@ -1,10 +1,12 @@
 use quanlysinhvien;
 select student.* from student where student.StudentName like "h%";
+
 select class.* from class where class.startdate between '2008-12-1' and '2008-12-31';
+
 select subject.* from subject where subject.credit between 3 and 5;
+
 SET SQL_SAFE_UPDATES = 0;
 update student set classid=2 where studentname='Hung';
-
 
 select studentname,subname,mark from student
 join mark on student.studentid=mark.studentid
