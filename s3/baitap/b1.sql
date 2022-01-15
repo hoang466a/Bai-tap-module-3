cd5bc170dd140fa3d0d96c8c492da6f2eb643eba
hoang466a <hoangtn96@gmail.com>
2022-01-15 21:58:06 +0700

    day bai bai tap ngay 15/01/2022

--- s3/baitap/b1.sql
+++ s3/baitap/b1.sql
@@ -4,3 +4,13 @@ select class.* from class where class.startdate between '2008-12-1' and '2008-12
 select subject.* from subject where subject.credit between 3 and 5;
 SET SQL_SAFE_UPDATES = 0;
 update student set classid=2 where studentname='Hung';
+
+
+select studentname,subname,mark from student
+join mark on student.studentid=mark.studentid
+join `subject` on subject.subid=mark.subid
+order by mark desc,studentname asc 
+
+
+
+
