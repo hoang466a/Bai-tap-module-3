a2630d2c782323d6f963c2d81b2d2b89f024a9d2
hoang466a <hoangtn96@gmail.com>
2022-01-16 10:35:25 +0700

    day bai tap ngay 16/01/2022

--- s3/baitap/b2.sql
+++ s3/baitap/b2.sql
@@ -1,24 +1,34 @@
 
 use order_product;
 create table customer(
-cID int primary key,
+cID int primary key auto_increment,
 `name` varchar(25),
 `cage` tinyint
 );
-
+delete `customer`.* from `customer`;
 insert into customer
-values(1,'Minh Quan',10),(2,'Ngoc Oanh',20),(3,'Hong Ha',50);
+values('Minh Quan',10),('Ngoc Oanh',20),('Hong Ha',50);
 
 create table `order`(
-oID int primary key,
+oID int primary key auto_increment,
 cID int,
 `oDate` datetime,
 oTotalPrice int,
 foreign key (cID) references customer(cID)
 );
+delete `order`.* from `order`;
+insert into `order`
+values(1,'2006-3-21',null),(2,'2006-3-23',null),(1,'2006-3-16',null);
+
+create table `product`(
+pID int primary key auto_increment, 
+pName varchar(25),
+pPrice int
+);
+
+delete `product`.* from `product`;
 
 insert into `order`
-values(1,1,'2006-3-21',null),(2,2,'2006-3-23',null),(3,1,'2006-3-16',null)
-
+values(1,`may giat`,3),(2,`may giat`,3),(1,`may giat`,3),(1,`may giat`,3),(1,`may giat`,3)
 
 
