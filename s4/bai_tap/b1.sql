6944390c5e8fc71a8464024eb5aca2b42b9702f6
hoang466a <hoangtn96@gmail.com>
2022-01-17 00:13:24 +0700

    day bai tap ngay 17/01/2022

---
+++ s4/bai_tap/b1.sql
@@ -0,0 +1,10 @@
+use quanlysinhvien;
+
+select * from `subject` order by `Credit` desc limit 1;
+
+select `subject`.subid,`subject`.subname,`subject`.Credit,`subject`.Status from `subject` left join `mark` on `subject`.subid=`mark`.subid order by `Mark` desc limit 1;
+
+select `student`.StudentId,`student`.StudentName,`student`.Address,`student`.phone,`student`.Status,`student`.classid,avg(`mark`.mark) as avg
+from `student`
+join `mark` on `student`.StudentId=`mark`.studentid
+group by `mark`.studentid
