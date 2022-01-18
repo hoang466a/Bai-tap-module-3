b844a39d5d1c77f97e5d84ada531f34deec30345
hoang466a <hoangtn96@gmail.com>
2022-01-18 17:19:03 +0700

    furama data

--- furama data/furama.sql
+++ furama data/furama.sql
@@ -41,7 +41,58 @@ ma_khach_hang int primary key auto_increment not null,
 ma_loai_khach int,
 ho_ten varchar(45) not null,
 ngay_sinh datetime not null,
-gioi_tinh bit not null,
- 
+gioi_tinh bit(1) not null,
+so_cmnd varchar(45) not null,
+so_dien_thoai varchar(45) not null,
+email varchar(45),
+dia_chi varchar(45),
+foreign key (ma_loai_khach) references loai_khach(ma_loai_khach)
 );
+
+create table dich_vu_di_kem(
+ma_dich_vu_di_kem int primary key auto_increment not null,
+ten_dich_vu_di_kem varchar(45) not null,
+gia double not null,
+don_vi varchar(10) not null,
+trang_thai varchar(45)
+);
+
+create table hop_dong_chi_tiet(
+ma_hop_dong_chi_tiet int primary key auto_increment not null,
+ma_hop_dong int,
+ma_dich_vu_di_kem int,
+so_luong int not null,
+foreign key(ma_hop_dong) references hop_dong(ma_hop_dong),
+foreign key(ma_dich_vu_di_kem) references dich_vu_di_kem(ma_dich_vu_di_kem)
+);
+
+create table kieu_thue(
+ma_kieu_thue int primary key auto_increment not null,
+ten_kieu_thue varchar(45)
+);
+
+create table loai_dich_vu(
+ma_loai_dich_vu int primary key auto_increment not null,
+ten_loai_dich_vu varchar(45)
+);
+
+create table dich_vu(
+ma_dich_vu int primary key auto_increment not null,
+ten_dich_vu varchar(45) not null,
+dien_tich int,
+chi_phi_thue double not null,
+so_nguoi_toi_da int, 
+ma_kieu_thue int,
+ma_loai_dich_vu int,
+tieu_chuan_phong varchar(45),
+mo_ta_tien_nghi_khac varchar(45),
+dien_tich_ho_boi double,
+so_tang int,
+foreign key (ma_kieu_thue) references kieu_thue(ma_kieu_thue),
+foreign key (ma_loai_dich_vu) references loai_dich_vu(ma_loai_dich_vu)
+);
+
+
+
+
 
