aada8f5aeb2a5130ad54a29e1950d629a4139f8c
hoang466a <hoangtn96@gmail.com>
2022-02-19 23:43:54 +0700

    day bai tap ngay 19-02-2022

---
+++ s12 JDBC&CRUD/thuc_hanh/user-manager/src/main/java/view/IUserDAO.java
@@ -0,0 +1,18 @@
+package view;
+
+import model.User;
+
+import java.util.List;
+import java.sql.SQLException;
+
+public interface IUserDAO {
+    public void insertUser(User user) throws SQLException;
+
+    public User selectUser(int id);
+
+    public List<User> selectAllUsers();
+
+    public boolean deleteUser(int id) throws SQLException;
+
+    public boolean updateUser(User user) throws SQLException;
+}
